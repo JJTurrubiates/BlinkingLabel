0904a9f10e40f621e7afc6eaa9e3ef798f238bae
TuRru <jesus@Turru.local>
2018-07-26 16:10:45 -0400

    Initial commit

---
+++ Example/BlinkingLabel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BlinkingLabel
+//
+//  Created by Jesus Turrubiates on 07/26/2018.
+//  Copyright (c) 2018 Jesus Turrubiates. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/BlinkingLabel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BlinkingLabel
+//
+//  Created by Jesus Turrubiates on 07/26/2018.
+//  Copyright (c) 2018 Jesus Turrubiates. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
